fca224b2cf4a8f9746a4bdab1098f6edaab2f3f2
Jamie Hewitt <jamiehewitt@protonmail.com>
2026-04-02 21:49:45 +0100

    fix: add Windows.h include to Source/Settings.cpp

--- Source/Settings.cpp
+++ Source/Settings.cpp
@@ -1,5 +1,9 @@
 #include "Settings.h"
 
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+
 #include <fstream>
 #include <rapidjson/document.h>
 #include <rapidjson/istreamwrapper.h>
