23c556ce4c11e6e0ffed547f3be789b863e0f968
Jamie Hewitt <jamiehewitt@protonmail.com>
2026-04-02 22:19:14 +0100

    feat: add logging, overlay, F12 toggle - Source/Profiling.cpp

--- Source/Profiling.cpp
+++ Source/Profiling.cpp
@@ -1,5 +1,7 @@
 #include "Profiling.h"
 #include "Settings.h"
+#include "Overlay.h"
+#include "Log.h"
 
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
@@ -64,30 +66,48 @@ static void mainLoop_hook(GameWorld* self, float time)
     QueryPerformanceCounter(&end);
     s_current.frameEnd = end.QuadPart;
 
+    // Compute frame timings
+    __int64 frameTotal = s_current.frameEnd - s_current.frameStart;
+    __int64 charsTotal = s_current.charsUpdate_end - s_current.charsUpdate_start;
+    __int64 charsUTTotal = s_current.charsUpdateUT_end - s_current.charsUpdateUT_start;
+    __int64 sysMsg = s_current.processSysMessages_end - s_current.processSysMessages_start;
+    __int64 killList = s_current.processKillList_end - s_current.processKillList_start;
+    __int64 daily = s_current.dailyUpdatesRan
+        ? (s_current.dailyUpdates_end - s_current.dailyUpdates_start) : 0;
+
+    float totalMs = (float)TicksToMs(frameTotal);
+    float charsMs = (float)TicksToMs(charsTotal);
+    float charsUTMs = (float)TicksToMs(charsUTTotal);
+    float sysMsgMs = (float)TicksToMs(sysMsg);
+    float killListMs = (float)TicksToMs(killList);
+    float dailyMs = (float)TicksToMs(daily);
+
+    // Get character count
+    int charCount = 0;
+    if (ou && ou->initialized)
+        charCount = (int)ou->getCharacterUpdateList().size();
+
     // Write frame data to CSV
     if (s_csvFile.is_open())
     {
-        __int64 frameTotal = s_current.frameEnd - s_current.frameStart;
-        __int64 charsTotal = s_current.charsUpdate_end - s_current.charsUpdate_start;
-        __int64 charsUTTotal = s_current.charsUpdateUT_end - s_current.charsUpdateUT_start;
-        __int64 sysMsg = s_current.processSysMessages_end - s_current.processSysMessages_start;
-        __int64 killList = s_current.processKillList_end - s_current.processKillList_start;
-        __int64 daily = s_current.dailyUpdatesRan
-            ? (s_current.dailyUpdates_end - s_current.dailyUpdates_start) : 0;
-
         s_csvFile
             << s_frameCount << ","
-            << TicksToMs(frameTotal) << ","
-            << TicksToMs(charsTotal) << ","
-            << TicksToMs(charsUTTotal) << ","
-            << TicksToMs(sysMsg) << ","
-            << TicksToMs(killList) << ","
-            << TicksToMs(daily) << "\n";
+            << totalMs << ","
+            << charsMs << ","
+            << charsUTMs << ","
+            << sysMsgMs << ","
+            << killListMs << ","
+            << dailyMs << ","
+            << charCount << "\n";
 
         // Flush periodically to avoid data loss on crash
         if (s_frameCount % 300 == 0)
             s_csvFile.flush();
     }
+
+    // Update overlay (main thread, safe for MyGUI)
+    PerfOverlay::Update(totalMs, charsMs, charsUTMs, sysMsgMs, killListMs, dailyMs, charCount);
+    PerfOverlay::CheckToggleKey();
 
     ++s_frameCount;
 }
@@ -165,7 +185,7 @@ void Profiling::Init()
         if (s_csvFile.is_open())
         {
             s_csvFile << "frame,total_ms,charsUpdate_ms,charsUpdateUT_ms,"
-                      << "processSysMessages_ms,processKillList_ms,dailyUpdates_ms\n";
+                      << "processSysMessages_ms,processKillList_ms,dailyUpdates_ms,char_count\n";
             DebugLog("[KenshiPerfMod] Profiling enabled, writing to " + path);
         }
         else
